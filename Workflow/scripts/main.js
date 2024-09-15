9d74631dd17631ff8879c00ad90cb4bec36f1259
ognistik <github@robertoberg.com>
2024-09-14 20:40:02 -0600

    Added Autotcompletes to script filter

--- Workflow/scripts/main.js
+++ Workflow/scripts/main.js
@@ -16,6 +16,7 @@ function run(argv) {
         return JSON.stringify({
             items: [{
                 type: 'default',
+                autocomplete: 'Activate Taptix',
                 title: "Activate Taptix",
                 subtitle: "Taptix is Not Currently Running",
                 arg: 'simple_activate',
@@ -33,6 +34,7 @@ function run(argv) {
     const items = [
         {
             type: 'default',
+            autocomplete: "Disable Taptix",
             title: "Disable Taptix",
             subtitle: "Turn off all sounds",
             arg: 'simple_deactivate',
@@ -46,30 +48,35 @@ function run(argv) {
         },
         {
             type: 'default',
+            autocomplete: "Set Keyboard Sound",
             title: "Set Keyboard Sound",
             subtitle: runCommand("get_keyboard"),
             arg: 'set_keyboard'
         },
         {
             type: 'default',
+            autocomplete: "Toggle Keyboard",
             title: "Toggle Keyboard",
             subtitle: runCommand("get_keyboard_volume") === "Keyboard is muted" ? "Unmute keyboard" : "Mute keyboard",
             arg: 'simple_toggle_keyboard'
         },
         {
             type: 'default',
+            autocomplete: "Set Mouse Sound",
             title: "Set Mouse Sound",
             subtitle: runCommand("get_mouse"),
             arg: 'set_mouse'
         },
         {
             type: 'default',
+            autocomplete: "Toggle Mouse",
             title: "Toggle Mouse",
             subtitle: runCommand("get_mouse_volume") === "Mouse is muted" ? "Unmute mouse" : "Mute mouse",
             arg: 'simple_toggle_mouse'
         },
         {
             type: 'default',
+            autocomplete: "Set Volume",
             title: "Set Volume",
             subtitle: runCommand("get_volume"),
             arg: 'set_volume',
